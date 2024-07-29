c15fabdbc7711d92a7b98ec08dc356ec64537079
Christina_Raichel_Francis <celestianc.73@gmail.com>
2024-07-29 15:10:33 +0100

    Feat: Moving data from GCS to Bigquery

--- CODE/gcs_to_bigquery.py
+++ CODE/gcs_to_bigquery.py
@@ -1,24 +1,31 @@
 from google.cloud import bigquery
 from google.cloud import storage
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 # Set up BigQuery client
 bigquery_client = bigquery.Client()
-dataset_id = 'news_dataset'
-table_id = 'news_data_table'
-
-GCS_BUCKET_NAME = os.getenv('GCS_BUCKET_NAME')
-GCS_FILE_PREFIX =  os.getenv('GCS_FILE_PREFIX')
+dataset_id = os.getenv('DATASET_ID')
+table_id = os.getenv('TABLE_ID')
 
 # GCS client
-storage_client = storage.Client()   
-bucket = storage_client.bucket(GCS_BUCKET_NAME)
+BUCKET_NAME = os.getenv('GCS_BUCKET_NAME')
+GCS_FILE_PREFIX =  os.getenv('GCS_FILE_PREFIX')
+print(BUCKET_NAME)
+
+storage_client = storage.Client(project= os.getenv('GCS_PROJECT_NAME'))
+bucket = storage_client.bucket(BUCKET_NAME)
 
 # Load files from GCS to BigQuery
 blobs = bucket.list_blobs(prefix=GCS_FILE_PREFIX)
+
 for blob in blobs:
     if blob.name.endswith('.json'):
-        uri = f"gs://{GCS_BUCKET_NAME}/{blob.name}"
+        uri = f"gs://{BUCKET_NAME}/{blob.name}"
+        print(uri)
         load_job = bigquery_client.load_table_from_uri(
             uri,
             f"{dataset_id}.{table_id}",
