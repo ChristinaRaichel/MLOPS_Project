f9981ba203f94cb62fb73c2909ba044c1d23448c
Christina_Raichel_Francis <celestianc.73@gmail.com>
2024-08-01 00:33:25 +0100

    spark job configs and bg table schema correction

--- CODE/gcs_to_bigquery.py
+++ CODE/gcs_to_bigquery.py
@@ -2,21 +2,22 @@ from google.cloud import bigquery
 from google.cloud import storage
 import os
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col
+from pyspark.sql.functions import col, explode
 from dotenv import load_dotenv
 
 load_dotenv()
 credentials = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
-
+project_id = os.getenv('GCS_PROJECT_ID')
 
 spark = SparkSession.builder \
         .appName("GCS to BigQuery ETL") \
+        .config('spark.jars.packages', 'com.google.cloud.spark:spark-bigquery-with-dependencies_2.12:0.30.0,com.google.cloud.bigdataoss:gcs-connector:hadoop3-2.2.24-shaded') \
+        .config('spark.sql.catalogImplementation', 'in-memory') \
         .config('spark.hadoop.fs.gs.impl', 'com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem') \
         .config('spark.hadoop.fs.AbstractFileSystem.gs.impl', 'com.google.cloud.hadoop.fs.gcs.GoogleHadoopFS') \
         .config('google.cloud.auth.service.account.enable', 'true') \
-        .config('google.cloud.auth.service.account.json.keyfile', credentials) \
+        .config('google.cloud.auth.service.account.json.keyfile', os.environ['GOOGLE_APPLICATION_CREDENTIALS']) \
         .getOrCreate()
-
 
 
 # Set up BigQuery clien
@@ -33,15 +34,20 @@ bucket = storage_client.bucket(BUCKET_NAME)
 
 # Load files from GCS to BigQuery
 blobs = bucket.list_blobs(prefix=GCS_FILE_PREFIX)
-#json_files = [f"gs://{BUCKET_NAME}/{blob.name}" for blob in blobs if blob.name.endswith('.json')]
-json_files = 'gs://news_api_stream_bucket/news-articles/2024-07-25T23:35:03.000000Z_de8333a6-dac7-45aa-a035-c32df5e6af81.json'
-print(json_files)
+json_files = [f"gs://{BUCKET_NAME}/{blob.name}" for blob in blobs if blob.name.endswith('.json')]
+#json_files = 'gs://news_api_stream_bucket/news-articles/2024-07-25T23:35:03.000000Z_de8333a6-dac7-45aa-a035-c32df5e6af81.json'
+#print(json_files)
 df = spark.read.json(json_files)
-df = df.select(col("title"), col("categories"), col("snippet"),col("description"),col("language") )
+df = df.select(col("title"), col("description"), col("snippet"),col("language"),col("published_at"),col("source"),col("categories"))
+
+df = df.withColumn("categories", explode(col("categories")))
+print(df.head(5))
+
 
 df.write \
 .format("bigquery") \
-.option("table", f"{dataset_id}.{table_id}") \
+.option("table", f"{project_id}:{dataset_id}.{table_id}") \
+.option("temporaryGcsBucket", os.getenv('GCS_BUCKET_NAME')) \
 .mode("append") \
 .save()
         
